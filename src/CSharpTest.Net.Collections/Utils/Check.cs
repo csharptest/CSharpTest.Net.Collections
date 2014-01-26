e5a15e64977b39543a728765217876ccd3516822
csharptest <github@csharptest.net>
2014-01-26 13:46:58 -0600

    Updated copy

--- src/CSharpTest.Net.Collections/Utils/Check.cs
+++ src/CSharpTest.Net.Collections/Utils/Check.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright 2008-2013 by Roger Knapp, Licensed under the Apache License, Version 2.0
+﻿#region Copyright 2008-2014 by Roger Knapp, Licensed under the Apache License, Version 2.0
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
