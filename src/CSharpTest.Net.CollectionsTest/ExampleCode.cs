f8410523c6d1a4424cf80754db82079e4ea42148
csharptest <github@csharptest.net>
2014-09-06 13:49:34 -0500

    Fix for buggy example code test

--- src/CSharpTest.Net.CollectionsTest/ExampleCode.cs
+++ src/CSharpTest.Net.CollectionsTest/ExampleCode.cs
@@ -12,29 +12,57 @@ namespace CSharpTest.Net.Library.Test
     [TestFixture]
     public class ExampleCode
     {
+        private class Counts
+        {
+            public int Queued = 0, Dequeued = 0;
+        }
+
         [Test]
         public void LurchTableDemo()
         {
+            var counts = new Counts();
             //Queue where producer helps when queue is full
-            using (var queue = new LurchTable<string, int>(LurchTableOrder.Insertion, 100))
+            using (var queue = new LurchTable<string, int>(LurchTableOrder.Insertion, 10))
             {
                 var stop = new ManualResetEvent(false);
-                queue.ItemRemoved += kv => Console.WriteLine("[{0}] - {1}", Thread.CurrentThread.ManagedThreadId, kv.Key);
+                queue.ItemRemoved += kv =>
+                    {
+                        Interlocked.Increment(ref counts.Dequeued);
+                        Console.WriteLine("[{0}] - {1}", Thread.CurrentThread.ManagedThreadId, kv.Key);
+                    };
                 //start some threads eating queue:
-                var thread = new Thread(() => { while (!stop.WaitOne(0)) queue.Dequeue(); })
+                var thread = new Thread(() => 
+                {
+                    while (!stop.WaitOne(0))
+                    {
+                        KeyValuePair<string, int> kv;
+                        while (queue.TryDequeue(out kv))
+                            continue;
+                    }
+                })
                     { Name = "worker", IsBackground = true };
                 thread.Start();
 
                 var names = Directory.GetFiles(Path.GetTempPath(), "*", SearchOption.AllDirectories);
-                if (names.Length <= 100) throw new Exception("Not enough trash in your temp dir.");
-                var loops = Math.Max(1, 10000/names.Length);
+                if (names.Length < 1) throw new Exception("Not enough trash in your temp dir.");
+                var loops = Math.Max(1, 100/names.Length);
                 for(int i=0; i < loops; i++)
                     foreach (var name in names)
+                    {
+                        Interlocked.Increment(ref counts.Queued);
                         queue[name] = i;
+                    }
 
+                //help empty the queue
+                KeyValuePair<string, int> tmp;
+                while (queue.TryDequeue(out tmp))
+                    continue;
+                //shutdown
                 stop.Set();
                 thread.Join();
             }
+
+            Assert.AreEqual(counts.Queued, counts.Dequeued);
         }
 
         [Test]
